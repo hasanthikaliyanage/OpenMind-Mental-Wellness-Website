b17707fe917f3623021181107bce4d92e25d647b
Hasanthika Liyanage <161589170+hasanthikaliyanage@users.noreply.github.com>
2026-06-01 15:52:59 +0530

    Update db.php

--- OMP Therapist/db.php
+++ OMP Therapist/db.php
@@ -1,8 +1,12 @@
 <?php
 require 'vendor/autoload.php';
 
-$client = new MongoDB\Client("mongodb://localhost:27017");
-$db = $client->OMP;   // Database
+$client = new MongoDB\Client(
+    "mongodb+srv://USERNAME:PASSWORD@openmindcluster.u3wdvre.mongodb.net/OMP?retryWrites=true&w=majority"
+);
+
+$db = $client->OMP;
+
 $therapistsCollection = $db->therapists;
 $bookingsCollection = $db->bookings;
 ?>
